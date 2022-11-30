af39b0873786043fa60dd05202a3b0a0cfc28910
Abu Bakar <absoud4@gmail.com>
2022-11-30 10:58:22 +0600

    website all most done

---
+++ src/Containers/Blog/Blog.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Blog.css';
+import { blog1, blog2, blog3, blog4, blog5 } from './imports';
+
+import { Article } from '../../Components';
+
+const Blog = () => {
+  return (
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_grouA">
+          <Article
+            imgUrl={blog1}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+        <div className="gpt3__blog-container_grouB">
+          <Article
+            imgUrl={blog2}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog3}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog4}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imgUrl={blog5}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
